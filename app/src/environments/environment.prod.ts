489732eeb7d9e20914868e1216ad2706bbd30fb4
prabanch <prabanchcv@gmail.com>
2024-02-26 14:15:46 +0530

    first commit frontend

---
+++ app/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    "name": "prod",
+    "properties": {
+        "production": true,
+        "ssdURL": "http://localhost:8081/api/",
+        "tenantName": "brototype",
+        "appName": "goodiesapp-frontend",
+        "namespace": "com.brototype.goodiesapp-frontend",
+        "useDefaultExceptionUI": true,
+        "isIDSEnabled": "false",
+        "webAppMountpoint": "web",
+        "NGFORAGE_MOBILE_DRIVER": "INDEXED_DB"
+    }
+}

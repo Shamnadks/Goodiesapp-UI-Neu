74c963df806603998e8ef2bec61368efc2d9fb5e
prabanch <prabanchcv@gmail.com>
2024-02-27 13:01:19 +0530

    applicant list cmpleted

--- app/src/app/config/declarations.ts
+++ app/src/app/config/declarations.ts
@@ -16,6 +16,8 @@ window['neutrinos'] = {
 };
 
 //CORE_REFERENCE_IMPORTS
+//CORE_REFERENCE_IMPORT-getApplicants
+import { getApplicants } from '../sd-services/getApplicants';
 //CORE_REFERENCE_IMPORT-scannerModalComponent
 import { scannerModalComponent } from '../components/Layouts/scannerModal.component';
 //CORE_REFERENCE_IMPORT-applicantListComponent
@@ -77,6 +79,8 @@ export const appProviders = [
   },
   NAuthGuardService,
   //CORE_REFERENCE_PUSH_TO_PRO_ARRAY
+//CORE_REFERENCE_PUSH_TO_PRO_ARRAY-getApplicants
+getApplicants,
 ];
 
 /**
--- app/src/app/n-services/service-caller.service.ts
+++ app/src/app/n-services/service-caller.service.ts
@@ -1,11 +1,12 @@
 // _neu_generated_code__dont_modify_directly_
 //append_imports_start
 
+import * as sd_IQSPuXuoYNiGfMAP from 'app/sd-services/getApplicants'; //_splitter_
 import { Injectable } from '@angular/core'; //_splitter_
 //append_imports_end
 @Injectable({ providedIn: 'root' })
 export class NeuServiceInvokerService {
-  constructor() {}
+  constructor(private sd_IQSPuXuoYNiGfMAP: sd_IQSPuXuoYNiGfMAP.getApplicants) {}
   invoke(
     injectedServiceId: string,
     methodName: string,
